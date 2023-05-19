f9ffe5b2986a07b2f4b8e8f8f592681f6bf96428
darikutelov <dari@kutelov.com>
2023-05-19 10:59:27 +0300

    Create product and category models

---
+++ Chefsplace/Core/domain/model/product/Product.swift
@@ -0,0 +1,57 @@
+//
+//  Product.swift
+//  Chefsplace
+//
+//  Created by Dariy Kutelov on 19.05.23.
+//
+
+import Foundation
+
+struct Product: Hashable, Identifiable, Codable {
+    let id: String
+    let name: String
+    let images: [String]
+    let price: Decimal
+    let reducedPrice: Decimal
+    let shortDescription: String
+    let category: ProductCategory
+    let onPromotion: Bool?
+    let barCode: String?
+    let productCode: String?
+    let weight: Double
+}
+
+/// Conform to hashable
+extension Product {
+    static func ==(lhs: Product, rhs: Product) -> Bool {
+        return lhs.id == rhs.id && lhs.name == rhs.name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+struct ProductDescription: Codable {
+    let content: String
+    let ingredients: String
+    let advantages: String
+    let storage: String
+    let nutritionalValues: NutritionalValue
+}
+
+struct NutritionalValue: Identifiable, Codable {
+    var id: String {
+        return label
+    }
+    
+    let label: String
+    let text: String
+}
+
+struct ProductCategory: Identifiable, Codable {
+    let id: String
+    let name: String
+    let imageUrl: String
+    let position: Int
+}
---
+++ Chefsplace/Core/utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Chefsplace
+//
+//  Created by Dariy Kutelov on 19.05.23.
+//
+
+import Foundation
+
+enum Constants {
+    enum General {}
+    
+    enum Api {
+        public static let baseUrl = "https://cryptic-oasis-81288.herokuapp.com/api/v1/"
+        // public static let baseUrl = "http://localhost:8000/api/v1/"
+    }
+}
