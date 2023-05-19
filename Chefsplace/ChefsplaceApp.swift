46c94181f02f511c046d9471298a469e66f75ca5
darikutelov <dari@kutelov.com>
2023-05-19 09:56:43 +0300

    Initial Commit

---
+++ Chefsplace/ChefsplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefsplaceApp.swift
+//  Chefsplace
+//
+//  Created by Dariy Kutelov on 19.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ChefsplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
